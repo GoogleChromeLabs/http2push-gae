cd7ecad9479af3f49a05425d2c523f3c86b7e6ab
Eric Bidelman <ebidel@gmail.com>
2015-09-08 15:26:08 -0700

    Initial commit
    Change-Id: I97a64a40f1e2135c0965b98bd615014539c5c904

---
+++ scripts/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# The directory in which this script resides.
+readonly BASEDIR=$(dirname $BASH_SOURCE)
+
+readonly STATICDIR=$BASEDIR/../static
+
+echo '=== Vulcanizing HTML Imports ==='
+vulcanize $STATICDIR/elements.html --inline-script --inline-css \
+    --strip-comments > $STATICDIR/elements.vulcanize.html
+
+echo '=== Generating HTTP2 push resource manifest ==='
+node $BASEDIR/generate_push_manifest.js $STATICDIR/ index.html
---
+++ scripts/generate_push_manifest.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// jshint node: true
+
+'use strict';
+
+var fs = require('fs');
+var listelements = require('./listelements');
+
+var PUSH_PRIORITY = 1; // TODO: this gives every resource priority 1.
+
+function generateAssociateContentHeader(urls) {
+  var associateContent = [];
+
+  for (var url in urls) {
+    var entry = '"' + url + '"';
+    var priority = urls[url];
+    if (priority !== null) {
+      entry += ':' +  priority;
+    }
+    associateContent.push(entry);
+  }
+
+  return associateContent.join(',');
+}
+
+listelements.list().then(function(urls) {
+  console.log('== Resource URLs in this app ==');
+  for (var i = 0, url; url = urls[i]; ++i) {
+    console.log('  ', url);
+  }
+
+  var priorityMapping = {};
+  urls.map(function(url, i) { priorityMapping[url] = PUSH_PRIORITY; });
+
+  var fileContent = JSON.stringify(priorityMapping, null, 2);
+  fs.writeFile('push_manifest.json', fileContent, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log('\nManifest file written!');
+  });
+
+});
+
+
